ddc1cf6ed6237a3997a3ac0998b305c915aaf0f9
RRuizD <rodrigo.ruizdecos@iesmiguelherrero.com>
2024-02-22 19:53:27 +0100

    ejemplo1 de Hola Mundo

---
+++ src/main/java/com/mycompany/contrato/Contrato.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contrato;
+
+/**
+ *
+ * @author Rodrigo Ruiz de Cos
+ */
+public class Contrato {
+    
+    //Creo los atributos del programa
+    private double costoLlamada;
+    private double costoMensaje;
+    private double Saldo;
+    
+    //Constructor por parametros
+    public Contrato(double costoLlamada, double costoMensaje, double Saldo) {
+        this.costoLlamada = costoLlamada;
+        this.costoMensaje = costoMensaje;
+        this.Saldo = Saldo;
+    }
+    
+    public double getSaldo() {
+        return this.Saldo;
+    }
+    
+    public double getcostoLlamada() {
+        return this.costoLlamada;
+    }
+    
+    public double getcostoMensaje() {
+        return this.costoMensaje;
+    }
+    
+    public void restarSaldo(double cantidad) {
+        //realizo la resta con un ternario(me parece mas rapido hacerlo asi)
+        Saldo = Saldo >= cantidad ? Saldo-cantidad : Saldo;
+    }
+}
